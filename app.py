7b4340fd6a6c62f09c7b60a045d400eb47b19794
lewwandi <intern1@zindhu.lk>
2025-06-17 01:48:48 +0530

    'build-web-app'

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template,request, redirect
+import numpy as np
+import string
+import re
+import pandas as pd
+import pickle
+from nltk.stem import PorterStemmer
 
 app = Flask(__name__)
 
+ps=PorterStemmer()
+
+#load the model
+with open('static/model/sentiment_model.pkl','rb') as file:
+    model=pickle.load(file)
+
+#load stopwords
+with open('static/model/corpora/stopwords/english','r') as file:
+    stopwords=file.read().splitlines()
+
+#load tokens
+vocab=pd.read_csv('static/model/vocabulary.txt',header=None)
+tokens=vocab[0].tolist()
 
 data = dict()
 reviews = []
@@ -13,23 +32,59 @@ def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
-
-
     return render_template('index.html', data=data)
+
+def remove_punctuations(text):
+    for punctuation in string.punctuation:
+        text=text.replace(punctuation,'')
+    return text
+
+#data preprocessing
+def data_preprocessing(text):
+ data=pd.DataFrame([text],columns=['tweet'])
+ data["tweet"]=data["tweet"].apply(lambda x:" ".join(x.lower() for x in x.split()))
+ data["tweet"]=data["tweet"].apply(lambda x: " ".join(re.sub(r'^https?:\/\/.*[\r\n]*', '', x,flags=re.MULTILINE) for x in x.split()))
+ data["tweet"]=data["tweet"].apply(remove_punctuations)
+ data["tweet"]=data["tweet"].str.replace('\d+', '', regex=True)  
+ data["tweet"]=data["tweet"].apply(lambda x: " ".join(x for x in x.split() if x not in stopwords))
+ data["tweet"]=data["tweet"].apply(lambda x: " ".join(ps.stem(x) for x in x.split()))
+ return data["tweet"]
+
+
+def vectorizer(ds):
+    vectorized_list=[]
+
+    for sentence in ds:
+        sentence_lst=np.zeros(len(tokens))
+
+        for i in range(len(tokens)):
+            if tokens[i] in sentence.split():
+                sentence_lst[i]=1
+
+        vectorized_list.append(sentence_lst)
+
+    vectorized_list_new=np.asarray(vectorized_list,dtype=np.float32)
+    return vectorized_list_new
+
+
+def get_prediction(vectorized_text):
+   prediction=model.predict(vectorized_text)
+   if prediction==1:
+      return 'Negative'
+   else:
+      return 'Positive'
+
+
+
+
 
 @app.route("/", methods = ['post'])
 def my_post():
     text = request.form['text']
-    logging.info(f'Text : {text}')
-
-    preprocessed_txt = preprocessing(text)
-    logging.info(f'Preprocessed Text : {preprocessed_txt}')
-
+    preprocessed_txt = data_preprocessing(text)
     vectorized_txt = vectorizer(preprocessed_txt)
-    logging.info(f'Vectorized Text : {vectorized_txt}')
-
     prediction = get_prediction(vectorized_txt)
-    logging.info(f'Prediction : {prediction}')
+  
 
     if prediction == 'negative':
         global negative
